414e9073c944d74a157b51c691380a753748a39c
Alan Ruiz <alan.c.ruiz.70@gmail.com>
2025-04-09 21:50:22 -0600

    Prueba

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,21 +1,32 @@
 import { Component } from '@angular/core';
+import { TaskListComponent } from './task-list/task-list.component';
+import { HeaderComponent } from "./header/header.component";
+
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
+  imports: [TaskListComponent, HeaderComponent],
 })
 export class AppComponent {
   title = 'angular-testing-project';
+
+
+
 
   addTask(task: string) {
 
   }
 
+
   deleteTask(task: string) {
+
   }
 
   getTasks(): string[] {
     return [];
   }
+
 }
--- src/app/task-list/task-list.component.ts
+++ src/app/task-list/task-list.component.ts
@@ -1,15 +1,46 @@
 import { Component } from '@angular/core';
+import { task } from '../models/task';
+import { FormsModule } from '@angular/forms';
+
 
 @Component({
   selector: 'app-task-list',
+  imports: [FormsModule],
   templateUrl: './task-list.component.html',
   styleUrls: ['./task-list.component.css']
 })
 export class TaskListComponent {
 
   tasks: string[] = [];
+  taskList:task[]=[];
+  newTask:task={
+    title:'',
+  };
 
-  constructor() {
+
+  addTask(task: void) {
+
+    if (this.newTask.title !=''&&  0)
+      {
+      this.taskList.push(this.newTask);
+      this.deleteTask();
+    }else alert('No se guardo');
+
+  }
+
+
+  deleteTask(task: void) {
+    this.newTask={
+      title:''
+    }
+  }
+
+  getTasks(): string[] {
+    return [];
+  }
+
+
+  /*constructor() {
     console.log('TaskListComponent initialized');
   }
 
@@ -24,4 +55,5 @@ export class TaskListComponent {
   deleteTask(task: string) {
     console.log('Deleting task:', task);
   }
+  */
 }
